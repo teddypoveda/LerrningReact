5cadd2104c387507241e802b2035cdacfd5d4da0
Teddy Poveda <tpoveda2@misena.edu.co>
2021-11-03 17:44:19 -0500

    GifExpertApp terminada

--- gitphy-search/src/Components/GifGrid.js
+++ gitphy-search/src/Components/GifGrid.js
@@ -1,23 +1,20 @@
-import React,{useState, useEffect} from 'react'
-import { getGifs } from '../helpers/getGifs';
+import React  from 'react'
 import { GifGridItem } from './GifGridItem';
-
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({category}) => {
 
-    const [images, setImages] = useState([]);
+    const {data:images, loading} = useFetchGifs(category);
 
-    useEffect( () => {
-        getGifs(category)
-        .then(imgs => setImages(imgs));
-        
-    }, [category])
+
+
 
     
 
     return (
         <>
-            <h3>{category}</h3>
+            <h3 className="animate__animated animate__fadeIn">{category}</h3>
+            {loading && <p className="animate__animated animate__flash">Loading</p>}
             <div className="card-grid">
                 
             
--- gitphy-search/src/Components/GifGridItem.js
+++ gitphy-search/src/Components/GifGridItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const GifGridItem = ({id,title,url}) => {
     return (
-        <div className="card">
+        <div className="card animate__animated animate__bounce">
             <img src={url} alt={title}></img>
             <p>{title}</p>
         </div>
